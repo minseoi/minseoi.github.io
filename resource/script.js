c720073adfc2d49e6d1aeddddd06bffb909a5ecc
minseoi <minseoi.cc@gmail.com>
2023-10-08 20:48:20 +0900

    update

--- resource/script.js
+++ resource/script.js
@@ -4,7 +4,7 @@ window.onscroll = function () {
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("header").style.top = "0";
     } else {
-        document.getElementById("header").style.top = "-4.2rem"; // 상단바 숨기기
+        document.getElementById("header").style.top = "-3.8rem"; // 상단바 숨기기
     }
     prevScrollpos = currentScrollPos;
 };
